81e1ba89945107f4c8ce80df26c746717c86616e
751150 <751150@PC332031.AIG-COGNIZANT.COM>
2019-01-14 15:22:57 +0530

    add

---
+++ src/test/java/com/security/oauth2ResourceServer/Oauth2ResourceServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.security.oauth2ResourceServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2ResourceServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
