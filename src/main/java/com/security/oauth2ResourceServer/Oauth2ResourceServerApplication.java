96be28bb3bc6d504d92675900bcda0071697b035
751150 <751150@PC332031.AIG-COGNIZANT.COM>
2019-01-18 16:23:41 +0530

    Test controller

--- src/main/java/com/security/oauth2ResourceServer/Oauth2ResourceServerApplication.java
+++ src/main/java/com/security/oauth2ResourceServer/Oauth2ResourceServerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan
 public class Oauth2ResourceServerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/security/oauth2ResourceServer/controller/Controller.java
@@ -0,0 +1,31 @@
+package com.security.oauth2ResourceServer.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+
+	@GetMapping
+	public String test() {
+		return "OK";
+	}
+	
+	@GetMapping(path="/hello",produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@PreAuthorize("hasRole('ROLE_VIEWER')")
+	public String test2() {
+		return "OK";
+	}
+	
+	@GetMapping(path="/helloAdmin",produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public String test3() {
+		return "OK";
+	}
+	
+
+}
